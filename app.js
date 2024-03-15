c4cdb609dd0f7e4aa4d1af3015df00057273fdeb
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-03-15 17:20:19 +0500

    add index and app.js files

---
+++ app.js
@@ -0,0 +1,137 @@
+
+//Q4
+
+// let vowel= prompt("Check is it vowel or consonant?");
+//  var vowel2= ("a")
+//  var vowel3= ("e")
+//  var vowel4= ("i")
+//  var vowel5= ("o")
+//  var vowel6= ("u")
+
+
+// if(vowel2=="a"|| vowel3=="e" || vowel4=="i" || vowel5 == "o" || vowel6=="u"){
+// 	document.write( "is a vowel letter")
+
+// }
+// else{
+// 	document.write("It is a consonat letter")
+// }
+
+
+
+//Q3
+
+// let integer = prompt("Enter any number for checking the positive or negative number?")
+
+// if(integer>0){
+// 	console.log( integer," is a positive number.")
+// }
+// else if (integer<0){
+// 	document.write( integer," is a negative number.")
+// }
+
+
+
+//Q5
+// let password = prompt("Enter Your password");
+// let samePassword = prompt("Enter the same password");
+
+
+// if (password===samePassword ){
+// 	console.log("Correct!")
+// }
+// else if (password!=samePassword){
+// 	console.log("Please enter your same password")
+// }
+// else{
+// 	console.log("Please enter your password")
+// }
+
+
+//Q6
+// var greeting;
+// var hour =13;
+
+
+
+// if (hour <18 ){
+// 	greeting="Good day";
+// 	else 
+// 		greeting= "Good evening";
+	
+// }	
+
+
+//Q7
+
+// var time = prompt("What is time now ?")
+// time=1900;
+
+// if (time>="0" && time>1200){
+// 	document.write("Good morning!")
+// }
+// else if (time>=1200 && time<1700){
+// 	document.write("Good afternoon!")
+// }
+// else if (time>=1700 && time<2100){
+// 	document.write("Good evening!")
+// }
+// else{
+// 	document.write("Good night!")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
